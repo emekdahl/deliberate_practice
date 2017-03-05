22c40b97a4d99b23bbf046ced6c689fc53e5a5a3
emekdahl <emily.ekdahl@gmail.com>
2017-03-05 14:04:15 -0600

    divide all numbers in an array by two

--- divide_by_two.rb
+++ divide_by_two.rb
@@ -1,1 +1,7 @@
-#divide every number in an array
+#divide every number in an array by 2
+
+numbers = [1,2,3,4,5]
+
+new_numbers = numbers.map {|number| number.to_f / 2}
+
+p new_numbers
--- double_every_other.rb
+++ double_every_other.rb
@@ -1,1 +1,15 @@
 #double every other array in an array of numbers
+
+numbers = [1, 2, 3, 4, 5]
+
+new_numbers = []
+
+loop do
+  # multiply the first item in the array by two
+
+  #insert that value back into the new array
+
+  #add add one to the index value
+
+  #push that value straight into the new array
+end
