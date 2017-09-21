b4cffedb7db78c910eff2b415928b855a9863ec4
emekdahl <emily.ekdahl@gmail.com>
2017-09-21 05:35:26 -0500

    ES6 map practice

--- filter_practice.js
+++ filter_practice.js
@@ -1,4 +1,4 @@
-var nums= [1,2,3,4];
+var nums = [1,2,3,4];
 
 var evens = nums.filter(num => num % 2 === 0);
 console.log(evens);
---
+++ mapPractice.js
@@ -0,0 +1,4 @@
+var nums = [1,2,3,4];
+
+var squares = nums.map(num => num * num);
+console.log(squares);
