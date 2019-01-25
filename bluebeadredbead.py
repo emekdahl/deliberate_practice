2f720ec549671a6a42f1b3e36fa748a40b363716
emekdahl <emily@otus.com>
2019-01-25 07:36:33 -0600

    red bead blue bead code wars kata in python solution

--- bluebeadredbead.py
+++ bluebeadredbead.py
@@ -5,13 +5,7 @@
 # Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
 # If there are less than 2 blue beads return 0.
 
-def count_red_beads(n):
-    blue_beads = n
-    if (blue_beads <= 2):
-      return 0
-    elif (blue_beads % 2 == 0):
-        return blue_beads * 2
-    else:
-        return ((blue_beads -1) * 2)
+def count_red_beads(nb):
+    return max(0, 2 * (nb - 1))
 
 count_red_beads(5)
